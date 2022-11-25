0814db7e442194cd469a524568e4ff8ef62b46ce
OmidOmar <a.omidomari@gmail.com>
2022-11-25 23:10:24 +0000

    lesson 2 build

---
+++ precache-manifest.b3e9bc222a8013f79f2d7da216947c2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7b66fa28971e490fa2914c0161c4d1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67a670d136c6c62fcd01",
+    "url": "/static/css/main.524e95f2.chunk.css"
+  },
+  {
+    "revision": "8b7a2cd579c2a301f6d0",
+    "url": "/static/js/2.5b7c11c6.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.5b7c11c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67a670d136c6c62fcd01",
+    "url": "/static/js/main.5f4decc0.chunk.js"
+  },
+  {
+    "revision": "4c7ee2dc1d082c23d2f6",
+    "url": "/static/js/runtime-main.42aab16e.js"
+  },
+  {
+    "revision": "dfcb1d152145a40767154916a7336c46",
+    "url": "/static/media/CYF_Hotel-logo.dfcb1d15.png"
+  }
+]);
